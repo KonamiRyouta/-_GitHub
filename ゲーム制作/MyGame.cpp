540a1a8bf6d1651fc1b59f7c3bd9491030e60f59
KonamiRyouta <hj-g19007@sist.ac.jp>
2020-11-30 12:27:48 +0900

    初めてのコミット

---
+++ ゲーム制作/MyGame.cpp
@@ -0,0 +1,423 @@
+#include"DxLib.h"
+
+#define GAME_WIDTH	1000
+#define GAME_HEIGHT 600
+#define GAME_COLOR	32
+
+#define GAME_WINDOW_BAR 0
+#define GAME_WINDOW_NAME "レトロアクション"
+
+#define GAME_FPS	60
+
+#define PATH_MAX	255
+
+#define IMAGE_LOAD_ERR_TITLE	TEXT("画像読み込みエラー")
+
+#define IMAGE_TITLE_BK_PATH			TEXT(".\\IMAGE\\タイトル.png")
+#define IMAGE_TITLE_ROGO_PATH		TEXT(".\\IMAGE\\タイトル_レトロアクション_3.png")
+#define IMAGE_TITLE_ROGO_ROTA		0.005
+#define IMAGE_TITLE_ROGO_ROTA_MAX	1.0
+#define IMAGE_TITLE_ROGO_X_SPEED	1
+#define IMAGE_TITLE_START_PATH		TEXT(".\\IMAGE\\title_start_1.png")
+#define IMAGE_TITLE_START_CNT		1
+#define IMAGE_TITLE_START_CNT_MAX	30
+
+enum GAME_SCENE {
+	GAME_SCENE_START,
+	GAME_SCENE_PLAY,
+	GAME_SCENE_END
+};
+
+typedef struct STRUCT_I_POINT
+{
+	int x = -1;
+	int y = -1;
+}iPOINT;
+
+typedef struct STRUCT_IMAGE
+{
+	char path[PATH_MAX];
+	int handle;
+	int x;
+	int y;
+	int width;
+	int height;
+}IMAGE;
+
+typedef struct STRUCT_IMAGE_ROTA
+{
+	IMAGE image;
+	double angle;
+	double angleMAX;
+	double rate;
+	double rateMAX;
+}IMAGE_ROTA;
+
+typedef struct STRUCT_IMAGE_BLINK
+{
+	IMAGE image;
+	int Cnt;
+	int CntMAX;
+	BOOL IsDraw;
+}IMAGE_BLINK;
+
+int StartTimeFps;
+int CountFps;
+float CalcFps;
+int SampleNumFps = GAME_FPS;
+
+char AllKeyState[256] = { '\0' };
+char OldAllKeyState[256] = { '\0' };
+
+int GameScene;
+
+IMAGE ImageTitleBK;
+IMAGE_ROTA ImageTitleROGO;
+IMAGE_BLINK ImageTitleSTART;
+
+VOID MY_FPS_UPDATE(VOID);
+VOID MY_FPS_DRAW(VOID);
+VOID MY_FPS_WAIT(VOID);
+
+VOID MY_ALL_KEYDOWN_UPDATE(VOID);
+BOOL MY_KEY_DOWN(int);
+BOOL MY_KEY_UP(int);
+BOOL MY_KEYDOWN_KEEP(int, int);
+
+VOID MY_START(VOID);
+VOID MY_START_PROC(VOID);
+VOID MY_START_DRAW(VOID);
+
+VOID MY_PLAY(VOID);
+VOID MY_PLAY_PROC(VOID);
+VOID MY_PLAY_DRAW(VOID);
+
+VOID MY_END(VOID);
+VOID MY_END_PROC(VOID);
+VOID MY_END_DRAW(VOID);
+
+BOOL MY_LOAD_IMAGE(VOID);
+VOID MY_DELETE_IMAGE(VOID);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCndShow)
+{
+	ChangeWindowMode(TRUE);
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);
+	SetWindowStyleMode(GAME_WINDOW_BAR);
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));
+	SetAlwaysRunFlag(TRUE);
+
+	if (DxLib_Init() == -1) { return-1; }
+
+	if (MY_LOAD_IMAGE() == FALSE) { return-1; }
+
+	GameScene = GAME_SCENE_START;
+
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (TRUE)
+	{
+		if (ProcessMessage() != 0) { break; }
+		if (ClearDrawScreen() != 0) { break; }
+		
+		MY_ALL_KEYDOWN_UPDATE();
+
+		MY_FPS_UPDATE();
+
+		switch (GameScene)
+		{
+		case GAME_SCENE_START:
+			MY_START();
+			break;
+		case GAME_SCENE_PLAY:
+			MY_PLAY();
+			break;
+		case GAME_SCENE_END:
+			MY_END();
+			break;
+		}
+
+		ScreenFlip();
+	}
+
+	MY_DELETE_IMAGE();
+
+	DxLib_End();
+
+	return 0;
+}
+
+VOID MY_FPS_UPDATE(VOID)
+{
+	if (CountFps == 0) //1フレーム目なら時刻を記憶
+	{
+		StartTimeFps = GetNowCount();
+	}
+
+	if (CountFps == SampleNumFps) //60フレーム目なら平均を計算
+	{
+		int now = GetNowCount();
+		//現在の時間から、0フレーム目の時間を引き、FPSの数値で割る＝1FPS辺りの平均時間が計算される
+		CalcFps = 1000.f / ((now - StartTimeFps) / (float)SampleNumFps);
+		CountFps = 0;
+		StartTimeFps = now;
+	}
+	CountFps++;
+	return;
+}
+
+VOID MY_FPS_WAIT(VOID)
+{
+	int resultTime = GetNowCount() - StartTimeFps;					//かかった時間
+	int waitTime = CountFps * 1000 / GAME_FPS - resultTime;	//待つべき時間
+
+	if (waitTime > 0)		//指定のFPS値よりも早い場合
+	{
+		WaitTimer(waitTime);	//待つ
+	}
+	return;
+}
+
+VOID MY_ALL_KEYDOWN_UPDATE(VOID)
+{
+	char TempKey[256];
+
+	for (int i = 0; i < 256; i++)
+	{
+		OldAllKeyState[i] = AllKeyState[i];
+	}
+
+	GetHitKeyStateAll(TempKey);
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (TempKey[i] != 0)
+		{
+			AllKeyState[i]++;
+		}
+		else
+		{
+			AllKeyState[i] = 0;
+		}
+	}
+	return;
+}
+
+BOOL MY_KEY_DOWN(int KEY_INPUT_)
+{
+	if (AllKeyState[KEY_INPUT_] != 0)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+BOOL MY_KEY_UP(int KEY_INPUT_)
+{
+	if (OldAllKeyState[KEY_INPUT_] >= 1
+		&& AllKeyState[KEY_INPUT_] == 0)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+BOOL MY_KEYDOWN_KEEP(int KEY_INPUT_, int DownTime)
+{
+	int UpdateTime = DownTime * GAME_FPS;
+
+	if (AllKeyState[KEY_INPUT_] > UpdateTime)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+VOID MY_START(VOID)
+{
+	MY_START_PROC();
+	MY_START_DRAW();
+
+	return;
+}
+
+VOID MY_START_PROC(VOID)
+{
+	if (MY_KEY_DOWN(KEY_INPUT_RETURN) == TRUE)
+	{
+		GameScene = GAME_SCENE_PLAY;
+	}
+
+	if (ImageTitleROGO.rate < ImageTitleROGO.rateMAX)
+	{
+		ImageTitleROGO.rate += IMAGE_TITLE_ROGO_ROTA;
+	}
+
+	if (ImageTitleROGO.image.x < GAME_WIDTH / 2)
+	{
+		ImageTitleROGO.image.x += IMAGE_TITLE_ROGO_X_SPEED;
+	}
+	else
+	{
+		if (ImageTitleSTART.Cnt < ImageTitleSTART.CntMAX)
+		{
+			ImageTitleSTART.Cnt += IMAGE_TITLE_START_CNT;
+		}
+		else
+		{
+			if (ImageTitleSTART.IsDraw == FALSE)
+			{
+				ImageTitleSTART.IsDraw = TRUE;
+			}
+			else if (ImageTitleSTART.IsDraw == TRUE)
+			{
+				ImageTitleSTART.IsDraw == FALSE;
+			}
+			ImageTitleSTART.Cnt = 0;
+		}
+	}
+
+	return;
+}
+
+VOID MY_START_DRAW(VOID)
+{
+	DrawGraph(ImageTitleBK.x, ImageTitleBK.y, ImageTitleBK.handle, TRUE);
+
+	DrawRotaGraph(
+		ImageTitleROGO.image.x, ImageTitleROGO.image.y,
+		ImageTitleROGO.rate,
+		ImageTitleROGO.angle,
+		ImageTitleROGO.image.handle, TRUE);
+
+	if (ImageTitleSTART.IsDraw == TRUE)
+	{
+		DrawGraph(ImageTitleSTART.image.x, ImageTitleSTART.image.y, ImageTitleSTART.image.handle, TRUE);
+	}
+
+	DrawString(0, 0, "スタート画面(エンターキーを押して)", GetColor(255, 255, 255));
+	return;
+}
+
+VOID MY_PLAY(VOID)
+{
+	MY_PLAY_PROC();
+	MY_PLAY_DRAW();
+
+	return;
+}
+
+VOID MY_PLAY_PROC(VOID)
+{
+	if (MY_KEY_DOWN(KEY_INPUT_SPACE) == TRUE)
+	{
+		GameScene = GAME_SCENE_END;
+	}
+
+	return;
+}
+
+VOID MY_PLAY_DRAW(VOID)
+{
+	DrawBox(10, 10, GAME_WIDTH - 10, GAME_HEIGHT - 10, GetColor(0, 255, 0), TRUE);
+
+	DrawString(0, 0, "プレイ画面(スペースーキーを押して)", GetColor(255, 255, 255));
+	return;
+}
+
+VOID MY_END(VOID)
+{
+	MY_END_PROC();
+	MY_END_DRAW();
+
+	return;
+}
+
+VOID MY_END_PROC(VOID)
+{
+	if (MY_KEY_DOWN(KEY_INPUT_ESCAPE) == TRUE)
+	{
+		GameScene = GAME_SCENE_START;
+	}
+
+	return;
+}
+
+VOID MY_END_DRAW(VOID)
+{
+	DrawBox(10, 10, GAME_WIDTH - 10, GAME_HEIGHT - 10, GetColor(0, 0, 255), TRUE);
+
+	DrawString(0, 0, "エンド画面(エスケープーキーを押して)", GetColor(255, 255, 255));
+	return;
+}
+
+BOOL MY_LOAD_IMAGE(VOID)
+{
+
+	//タイトル背景
+	strcpy_s(ImageTitleBK.path, IMAGE_TITLE_BK_PATH);			//パスの設定
+	ImageTitleBK.handle = LoadGraph(ImageTitleBK.path);			//読み込み
+	if (ImageTitleBK.handle == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), IMAGE_TITLE_BK_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+	GetGraphSize(ImageTitleBK.handle, &ImageTitleBK.width, &ImageTitleBK.height);	//画像の幅と高さを取得
+	ImageTitleBK.x = GAME_WIDTH / 2 - ImageTitleBK.width / 2;		//左右中央揃え
+	ImageTitleBK.y = GAME_HEIGHT / 2 - ImageTitleBK.height / 2;		//上下中央揃え
+
+	//タイトルロゴ
+	strcpy_s(ImageTitleROGO.image.path, IMAGE_TITLE_ROGO_PATH);					//パスの設定
+	ImageTitleROGO.image.handle = LoadGraph(ImageTitleROGO.image.path);			//読み込み
+	if (ImageTitleROGO.image.handle == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), IMAGE_TITLE_ROGO_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+	GetGraphSize(ImageTitleROGO.image.handle, &ImageTitleROGO.image.width, &ImageTitleROGO.image.height);	//画像の幅と高さを取得
+	ImageTitleROGO.image.x = 230;							//光っている部分から描画したい
+	ImageTitleROGO.image.y = GAME_HEIGHT / 2;				//中央寄せ
+	ImageTitleROGO.angle = DX_PI * 2;						//回転率
+	ImageTitleROGO.angleMAX = DX_PI * 2;					//回転率MAX
+	ImageTitleROGO.rate = 0.0;								//拡大率
+	ImageTitleROGO.rateMAX = IMAGE_TITLE_ROGO_ROTA_MAX;		//拡大率MAX
+
+	//タイトルスタート
+	strcpy_s(ImageTitleSTART.image.path, IMAGE_TITLE_START_PATH);					//パスの設定
+	ImageTitleSTART.image.handle = LoadGraph(ImageTitleSTART.image.path);			//読み込み
+	if (ImageTitleSTART.image.handle == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), IMAGE_TITLE_START_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+	GetGraphSize(ImageTitleSTART.image.handle, &ImageTitleSTART.image.width, &ImageTitleSTART.image.height);	//画像の幅と高さを取得
+	ImageTitleSTART.image.x = GAME_WIDTH / 2 - ImageTitleSTART.image.width / 2;					//左右中央揃え
+	ImageTitleSTART.image.y = ImageTitleROGO.image.y + ImageTitleROGO.image.height / 2 + 10;	//ロゴの下に表示
+	ImageTitleSTART.Cnt = 0.0;								//カウンタ
+	ImageTitleSTART.CntMAX = IMAGE_TITLE_START_CNT_MAX;		//カウンタMAX
+	ImageTitleSTART.IsDraw = FALSE;							//描画させない
+
+	return TRUE;
+}
+
+//画像をまとめて削除する関数
+VOID MY_DELETE_IMAGE(VOID)
+{
+	DeleteGraph(ImageTitleBK.handle);
+	DeleteGraph(ImageTitleROGO.image.handle);
+	DeleteGraph(ImageTitleSTART.image.handle);
+
+	return;
+}
